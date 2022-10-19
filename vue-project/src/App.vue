462134793e41e733d58b4e992138d469982a6df5
irinamoondo <92740353+irinamoondo@users.noreply.github.com>
2022-10-19 21:34:26 +0200

    versión 19-10

---
+++ vue-project/src/App.vue
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<template>
+  <header class="flex flex-col items-center py-5 gap gap-3">
+    <img src ="../public/shield.svg" alt="" class="text-2xl"/>
+    <h1 class="uppercase text-5xl">Brewdog</h1>
+  </header>
+</template>
+
+<style>
+</style>
