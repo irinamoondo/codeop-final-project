baa76df316da659e8ff83fa20db6898beb921ccf
irinamoondo <92740353+irinamoondo@users.noreply.github.com>
2022-10-31 17:52:42 +0100

    versión 31-10

--- vue-project/src/views/Beer.vue
+++ vue-project/src/views/Beer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col items-center justify-center gap gap-3 p-8">
+    <div class="flex flex-col items-center justify-center gap gap-3 p-6 text-md">
 
         <div v-if="loading">
             <img src="https://thumbs.gfycat.com/ShrillInfamousDodobird-size_restricted.gif" class="animate-spin"/>
@@ -9,23 +9,23 @@
             <p class="text-red-500">{{error}}</p>
         </div>
 
-        <main v-if="data" class="flex items-center justify-center gap gap-5">
-            <img class="w-1/3" :src=data.image_url alt={{data.name}}/>
-            <div class="flex flex-col items-center justify-center w-2/3">
-                <h1>{{data.name}}</h1>
-                <p>ABV: {{data.abv}}%</p>
-                <p>IBU: {{data.ibu}}</p>
-                <p>{{data.description}}</p>
-                <p class="font-bold italic">Pair it with:</p>
-                <ul v-for="(item, index) in data.food_pairing" :key="index"> 
-                    <li>{{item}}</li>
-                </ul>
-            </div>
+        <main v-if="data" class="flex flex-col items-center justify-center gap gap-3 bg-neutral-100 p-3 w-80 text-center text-xs">
+          <h1 class="text-lg">{{data.name}}</h1>
+          <p>ABV: {{data.abv}}%</p>
+          <p>IBU: {{data.ibu}}</p>
+          <p class="p-3">{{data.description}}</p>
+          <p class="font-bold italic">Pair it with:</p>
+          <ul v-for="(item, index) in data.food_pairing" :key="index"> 
+            <li>{{item}}</li>
+          </ul>
+          <img :src=data.image_url alt={{data.name}}/>
         </main>
-
-        <router-link to="/">Go to Home</router-link>
-        <router-view></router-view>
+        
+        <div class="p-8 text-xs text-purple-700 hover:text-purple-500">
+          <router-link to="/">Home</router-link>
+          <router-view></router-view>
         </div>
+      </div>
 </template>
 
 <script>
